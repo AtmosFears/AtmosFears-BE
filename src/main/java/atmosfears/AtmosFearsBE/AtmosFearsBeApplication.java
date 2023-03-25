40b7371e47c35d64d1b52a6cf867673558f7d343
Paweł Gorgolewski <pgorgolewski01@gmail.com>
2023-03-25 14:29:03 +0100

    Spring boot init

---
+++ src/main/java/atmosfears/AtmosFearsBE/AtmosFearsBeApplication.java
@@ -0,0 +1,13 @@
+package atmosfears.AtmosFearsBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmosFearsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmosFearsBeApplication.class, args);
+	}
+
+}
